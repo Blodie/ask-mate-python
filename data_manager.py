65567294dc5a578c150f3ee6b7a3be9e1d93e8db
Benjamin Kovacs <kojokobe@gmail.com>
2019-01-21 13:42:06 +0100

    skeleton code ready, necessary .py and .html files added

---
+++ data_manager.py
@@ -0,0 +1,5 @@
+def get_table_from_file(filename):
+    pass
+
+def write_table_to_file(filename):
+    pass
--- server.py
+++ server.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template, redirect, request
 app = Flask(__name__)
 
+@app.route('/list')
 @app.route('/')
 def route_index():
     return render_template('index.html')
+
+@app.route('/question/<question_id>')
+def see_question(question_id):
+    return render_template('question.html', id=question_id)
+
+@app.route('/add-question')
+def ask_question():
+    return render_template('ask_question.html')
+
+@app.route('question/<question_id>/new-answer')
+def answer(question_id):
+    return render_template('answer.html', id=question_id)
 
 if __name__ == "__main__":
     app.run(
