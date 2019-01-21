5d699e3b5e8c495d64b6943c6b9c7e7f2c15482d
Benjamin Kovacs <kojokobe@gmail.com>
2019-01-21 14:43:58 +0100

    data_manager.py : import, export functions are both operational

--- data_manager.py
+++ data_manager.py
@@ -3,14 +3,19 @@ from csv import DictReader, DictWriter
 def get_table_from_file(filename):
     with open(filename) as f:
         fieldnames = f.readline().rstrip().split(",")
-        table = []
-        for row in DictReader(f, fieldnames=fieldnames):
-            table.append(dict(row))
-    return table
-            
+        return [dict(row) for row in DictReader(f, fieldnames=fieldnames)]
 
-def write_table_to_file(filename):
-    pass
+def write_table_to_file(filename, table):
+    with open(filename, "w", newline = "") as f:
+        writer = DictWriter(f, fieldnames=list(table[0].keys()))
+        writer.writeheader()
+        for item in table:
+            writer.writerow(item)
+
 
 if __name__ == "__main__":
-    print(get_table_from_file("./sample_data/question.csv"))
+    # print(get_table_from_file("./sample_data/answer.csv"))
+    # print(get_table_from_file("./sample_data/question.csv"))
+    # print("\n")
+    # write_table_to_file("./sample_data/test_export.csv", get_table_from_file("./sample_data/question.csv"))
+    # print(get_table_from_file("./sample_data/test_export.csv"))
