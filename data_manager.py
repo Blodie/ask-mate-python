0a3df55bb8b0c559fddd757bea5cb5feff4413d1
Benjamin Kovacs <kojokobe@gmail.com>
2019-01-21 14:28:07 +0100

    Can import list of dictionaries from csv files

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,16 @@
+from csv import DictReader, DictWriter
+
 def get_table_from_file(filename):
-    pass
+    with open(filename) as f:
+        fieldnames = f.readline().rstrip().split(",")
+        table = []
+        for row in DictReader(f, fieldnames=fieldnames):
+            table.append(dict(row))
+    return table
+            
 
 def write_table_to_file(filename):
     pass
+
+if __name__ == "__main__":
+    print(get_table_from_file("./sample_data/question.csv"))
--- server.py
+++ server.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, redirect, request
+from data_manager import get_table_from_file, write_table_to_file
 app = Flask(__name__)
+
+question_headers = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
+answer_headers = ["id", "submission_time", "vote_number", "question_id", "message", "image"]
+
+questions = get_table_from_file("question.csv")
+answers = get_table_from_file("answer.csv")
 
 @app.route('/list')
 @app.route('/')
@@ -14,7 +21,7 @@ def see_question(question_id):
 def ask_question():
     return render_template('ask_question.html')
 
-@app.route('question/<question_id>/new-answer')
+@app.route('/question/<question_id>/new-answer')
 def answer(question_id):
     return render_template('answer.html', id=question_id)
 
