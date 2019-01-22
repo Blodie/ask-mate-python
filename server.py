ce41deb1f6c6972809cd97d69c569e31dff7fc22
Benjamin Kovacs <kojokobe@gmail.com>
2019-01-22 10:40:48 +0100

    minor refactor of index.html and route_index()

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request
 from data_manager import get_table_from_file, write_table_to_file
-import time
+from time import strftime, localtime
 app = Flask(__name__)
 
 question_headers = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
@@ -12,23 +12,23 @@ answers = get_table_from_file("./sample_data/answer.csv")
 @app.route('/list')
 @app.route('/')
 def route_index():
-    table = [{key:value for key, value in question.items() if key in question_headers[:-2]} for question in questions]
+    questions_filtered = [{key:value for key, value in question.items() if key in question_headers[:-2]} for question in questions]
     try:
-        for question in table:
-            question["submission_time"] = time.strftime("%D %H:%M", time.localtime(int(question["submission_time"])))
+        for question in questions_filtered:
+            question["submission_time"] = strftime("%D %H:%M", localtime(int(question["submission_time"])))
     except:
         pass
     finally:
-        return render_template('index.html', table=table)
+        return render_template('index.html', questions=questions_filtered)
 
-@app.route('/question/<question_id>')   
+@app.route('/question/<question_id>')
 def see_question(question_id):
     question = next((question for question in questions if question["id"] == question_id), None)
     answers_for_this_question = [answer for answer in answers if answer["question_id"] == question_id]
     try:
-        question["submission_time"] = time.strftime("%D %H:%M", time.localtime(int(question["submission_time"])))
+        question["submission_time"] = strftime("%D %H:%M", localtime(int(question["submission_time"])))
         for answer in answers_for_this_question:
-            answer["submission_time"] = time.strftime("%D %H:%M", time.localtime(int(answer["submission_time"])))
+            answer["submission_time"] = strftime("%D %H:%M", localtime(int(answer["submission_time"])))
     except:
         pass
     finally:
