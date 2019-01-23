b6473c439fc27e7175a99898406ee3c56c5ccb53
Benjamin Kovacs <kojokobe@gmail.com>
2019-01-23 10:08:25 +0100

    Answers can now be deleted individually on /question/<question_id> pages

--- data_manager.py
+++ data_manager.py
@@ -1,15 +1,15 @@
 from connection import get_table_from_file, write_table_to_file
 from util import convert_table_values_to_int, convert_unix_timestamp_to_real_date_in_table, convert_real_date_to_unix_timestamp_in_table
 
-def get_questions_table():
+def get_questions():
     return convert_unix_timestamp_to_real_date_in_table(convert_table_values_to_int(get_table_from_file("./sample_data/question.csv")))
 
-def get_answers_table():
+def get_answers():
     return convert_unix_timestamp_to_real_date_in_table(convert_table_values_to_int(get_table_from_file("./sample_data/answer.csv")))
 
-def save_questions_table(table):
+def save_questions(table):
     write_table_to_file("./sample_data/question.csv", convert_real_date_to_unix_timestamp_in_table(table))
 
-def save_answers_table(table):
+def save_answers(table):
     write_table_to_file("./sample_data/answer.csv", convert_real_date_to_unix_timestamp_in_table(table))
 
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, request
-from data_manager import get_questions_table, get_answers_table, save_answers_table, save_questions_table
+from data_manager import get_questions, get_answers, save_answers, save_questions
 app = Flask(__name__)
 
 # question_headers = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/list')
 @app.route('/')
 def route_index():
-    questions = get_questions_table()
+    questions = get_questions()
     order_by = request.args.get('order_by') if request.args.get('order_by') else "id"
     direction = True if request.args.get('order_direction') == 'desc' else False
     necessary_headers = list(questions[0].keys())[:-2] if questions else None
@@ -18,8 +18,8 @@ def route_index():
 
 @app.route('/question/<question_id>')
 def see_question(question_id):
-    questions = get_questions_table()
-    answers = get_answers_table()
+    questions = get_questions()
+    answers = get_answers()
     question = next((question for question in questions if question["id"] == int(question_id)), None)
     answers_for_this_question = [answer for answer in answers if answer["question_id"] == int(question_id)]
     return render_template('question.html', question=question, answers=answers_for_this_question)
@@ -34,13 +34,23 @@ def answer(question_id):
 
 @app.route('/question/<question_id>/delete')
 def delete_question(question_id):
-    questions = get_questions_table()
-    answers = get_answers_table()
+    questions = get_questions()
+    answers = get_answers()
     questions = [question for question in questions if question["id"] != int(question_id)]
     answers = [answer for answer in answers if answer["question_id"] != int(question_id)]
-    save_questions_table(questions)
-    save_answers_table(answers)
+    save_questions(questions)
+    save_answers(answers)
     return redirect('/')
+
+@app.route('/answer/<answer_id>/delete')
+def delete_answer(answer_id):
+    questions = get_questions()
+    answers = get_answers()
+    answer_to_be_deleted = next((answer for answer in answers if answer["id"] == int(answer_id)), None)
+    question_id = str(next((question for question in questions if question["id"] == answer_to_be_deleted["question_id"]), None)["id"])
+    answers = [answer for answer in answers if answer["id"] != int(answer_id)]
+    save_answers(answers)
+    return redirect(f"/question/{question_id}")
 
 if __name__ == "__main__":
     app.run(
