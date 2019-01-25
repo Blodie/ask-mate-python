803c42918d645dc8a6966375a545ff8fe6e7c36c
Benjamin Kovacs <kojokobe@gmail.com>
2019-01-25 11:25:41 +0100

    minor refactor: casting variables in decorators rather than in functions

--- server.py
+++ server.py
@@ -10,19 +10,18 @@ def route_index():
     order_by = request.args.get('order_by') if request.args.get('order_by') else "id"
     direction = True if request.args.get('order_direction') == 'desc' else False
     necessary_headers = list(questions[0].keys())[:-2] if questions else None
-    questions_filtered = [{key:value for key, value in question.items() if key in necessary_headers} for question in questions]
-    questions_filtered = sorted(questions_filtered, key = lambda x: x[order_by], reverse = direction)
-    return render_template('index.html', questions=questions_filtered, direction=direction)
+    questions = sorted(questions, key = lambda x: x[order_by], reverse = direction)
+    return render_template('index.html', questions=questions, headers=necessary_headers, direction=direction)
 
-@app.route('/question/<question_id>')
+@app.route('/question/<int:question_id>')
 def see_question(question_id):
     questions = get_questions()
     answers = get_answers()
-    i = next((i for i, question in enumerate(questions) if question["id"] == int(question_id)))
+    i = next((i for i, question in enumerate(questions) if question["id"] == question_id))
     if not request.args.get('inc'):
         questions[i]["view_number"] += 1
         save_questions(questions)
-    answers_for_this_question = [answer for answer in answers if answer["question_id"] == int(question_id)]
+    answers_for_this_question = [answer for answer in answers if answer["question_id"] == question_id]
     return render_template('question.html', question=questions[i], answers=answers_for_this_question)
 
 @app.route('/add-question', methods=['GET', 'POST'])
@@ -42,10 +41,10 @@ def ask_question():
         return redirect(f'/question/{new_ask["id"]}')
     return render_template('add_question.html', question=None)
 
-@app.route('/question/<question_id>/edit', methods=['GET', 'POST'])
+@app.route('/question/<int:question_id>/edit', methods=['GET', 'POST'])
 def edit_question(question_id):
     questions = get_questions()
-    i = next((i for i, question in enumerate(questions) if question["id"] == int(question_id)), None)
+    i = next((i for i, question in enumerate(questions) if question["id"] == question_id), None)
     if request.method == "POST":
         questions[i]["title"] = request.form["title"]
         questions[i]["message"] = request.form["msg"]
@@ -69,39 +68,39 @@ def answer(question_id):
         return redirect(f'/question/{question_id}?inc=False')
     return render_template('answer.html', id=question_id)
 
-@app.route('/question/<question_id>/delete')
+@app.route('/question/<int:question_id>/delete')
 def delete_question(question_id):
     questions = get_questions()
     answers = get_answers()
-    questions = [question for question in questions if question["id"] != int(question_id)]
-    answers = [answer for answer in answers if answer["question_id"] != int(question_id)]
+    questions = [question for question in questions if question["id"] != question_id]
+    answers = [answer for answer in answers if answer["question_id"] != question_id]
     save_questions(questions)
     save_answers(answers)
     return redirect('/')
 
-@app.route('/answer/<answer_id>/delete')
+@app.route('/answer/<int:answer_id>/delete')
 def delete_answer(answer_id):
     questions = get_questions()
     answers = get_answers()
-    question_id = next((answer for answer in answers if answer["id"] == int(answer_id)), None)["question_id"]
-    answers = [answer for answer in answers if answer["id"] != int(answer_id)]
+    question_id = next((answer for answer in answers if answer["id"] == answer_id), None)["question_id"]
+    answers = [answer for answer in answers if answer["id"] != answer_id]
     save_answers(answers)
     return redirect(f"/question/{question_id}?inc=False")
 
-@app.route('/question/<question_id>/vote-<vote>')
+@app.route('/question/<int:question_id>/vote-<vote>')
 def vote_on_question(question_id, vote):
     vote = True if vote == "up" else False
     questions = get_questions()
-    i = next((i for i, question in enumerate(questions) if question["id"] == int(question_id)), None)
+    i = next((i for i, question in enumerate(questions) if question["id"] == question_id), None)
     questions[i]["vote_number"] += 1 if vote else -1
     save_questions(questions)
     return redirect(f'/question/{questions[i]["id"]}?inc=False')
 
-@app.route('/answer/<answer_id>/vote-<vote>')
+@app.route('/answer/<int:answer_id>/vote-<vote>')
 def vote_on_answer(answer_id, vote):
     vote = True if vote == "up" else False
     answers = get_answers()
-    i = next((i for i, answer in enumerate(answers) if answer["id"] == int(answer_id)), None)
+    i = next((i for i, answer in enumerate(answers) if answer["id"] == answer_id), None)
     answers[i]["vote_number"] += 1 if vote else -1
     save_answers(answers)
     return redirect(f'/question/{answers[i]["question_id"]}?inc=False')
